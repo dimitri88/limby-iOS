ab16888178dbd7c17a5134511c8be75ed78d98bb
Unknown <linzuolee@gmail.com>
2018-05-16 16:56:46 -0700

    Added register device controller. For set up wizard

--- LIMBY/LIMBY/RegisterDeviceController.swift
+++ LIMBY/LIMBY/RegisterDeviceController.swift
@@ -7,9 +7,36 @@
 //
 
 import UIKit
-class RegisterDeviceController: UIViewController, ParticleSetupMainControllerDelegate {
+class RegisterDeviceController: UIViewController, ParticleSetupMainControllerDelegate{
     
     func particleSetupViewController(_ controller: ParticleSetupMainController!, didFinishWith result: ParticleSetupMainControllerResult, device: ParticleDevice!) {
+        switch result
+        {
+        case .success:
+            print("Setup completed successfully")
+        case .failureConfigure:
+            fallthrough
+        case .failureCannotDisconnectFromDevice:
+            fallthrough
+        case .failureLostConnectionToDevice:
+            fallthrough
+        case .failureClaiming:
+            print("Setup failed")
+        case .userCancel :
+            print("User cancelled setup")
+        case .loggedIn :
+            print("User is logged in")
+        default:
+            print("Uknown setup error")
+            
+        }
+        
+        if device != nil
+        {
+            device.getVariable("test", completion: { (value, err) -> Void in
+                
+            })
+        }
     }
     
     override func viewDidLoad() {
@@ -26,8 +53,22 @@ class RegisterDeviceController: UIViewController, ParticleSetupMainControllerDel
         dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func SetUpDevice(_ sender: Any) {
-        var setupController = ParticleSetupMainController()
-        self.present(setupController!, animated: true, completion: nil)
+    @IBAction func Setup(_ sender: Any) {
+        if let setupController = ParticleSetupMainController()
+        {
+            let customization = ParticleSetupCustomization.sharedInstance
+            customization().deviceName = "Perch"
+            customization().brandName = "Limby"
+            let image = UIImage(named: "bird.jpg")
+            let themeColor = UIColor(displayP3Red: 0.063, green: 0.565, blue: 0.741, alpha: 1.0)
+            customization().brandImage = image
+            customization().elementBackgroundColor = themeColor
+            customization().normalTextColor = themeColor
+            customization().lightStatusAndNavBar = false
+            setupController.delegate = self
+            self.present(setupController, animated: true, completion: nil)
+        }
     }
+    
+    
 }
